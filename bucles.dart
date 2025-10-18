ad2e5740fc7bdfd60855c7872d673c7e6f0e96fc
Esther Uño Soto <esther14uno@gmail.com>
2025-10-17 20:20:42 -0400

    clase2dart

---
+++ bucles.dart
@@ -0,0 +1,15 @@
+void main (){
+//  bucles while 
+// suma de los primeros 10 numeros
+
+int suma = 0;
+int contador =0;
+  while (contador <= 10){
+    suma = suma + contador;
+    contador = contador + 1;
+    
+  }
+  print(suma);
+}
+
+
---
+++ funciones.dart
@@ -0,0 +1,25 @@
+void main(){
+  saludo();
+  int resp = multiplicacion(10,5);
+  print(resp);
+  sumar2(12,6);
+  
+}
+
+//funcion sin retorno
+void saludo(){
+  print("Hola");
+}
+
+int sumar (int num1,int num2){
+  return num1 + num2;
+}
+
+int sumar2(int num1, int num2) => num1 + num2;
+
+// multiplicacion 
+int multiplicacion(int num1,int num2) => num1 * num2;
+
+int resta (int num1, int num2) => num1 - num2;
+
+double division (double num1, double num2) => num1 / num2;
